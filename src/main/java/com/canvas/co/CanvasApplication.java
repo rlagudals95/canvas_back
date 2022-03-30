613f95d4c2d5f869abaa4548d00d879a81691db4
rlagudals95 <ahovmf@naver.com>
2022-03-30 14:55:42 +0900

    first commit

---
+++ src/main/java/com/canvas/co/CanvasApplication.java
@@ -0,0 +1,13 @@
+package com.canvas.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanvasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanvasApplication.class, args);
+	}
+
+}
